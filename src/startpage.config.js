7b1228b226f8fc66056e4fe9205961e162a8be10
roinu176 <60661004+roinu176@users.noreply.github.com>
2021-02-19 00:20:45 +0800

    Update startpage.config.js

--- src/startpage.config.js
+++ src/startpage.config.js
@@ -1,37 +1,22 @@
 let config = {
   terminal: {
-    title: `Terminal at i@186526.xyz`,
+    title: `Terminal at 大咖流`,
     user: 'i',
-    hostname: '186526.xyz',
-    welcome: 'Welcome to <d color="#00f501">186526.xyz</d> terminal',
+    hostname: 'dakaliu',
+    welcome: 'Welcome to <d color="#00f501">大咖流</d> terminal',
     actions: [],
   },
   friends: [
     {
-      name: 'Skrshadow',
-      description: '是红雨 昆卡昆卡斯哈斯哈',
-      url: 'https://blog.skrshadown.cn',
-    },
-    {
-      name: 'Radium-Bit',
-      description: '在B站py友链的朋友（',
-      url: 'https://radium-bit.github.io/',
-    },
-    {
-      name: `JimmyQin's Blog`,
-      description: '是大佬的说',
-      url: 'https://www.jimmyqin.cn/',
-    },
-    {
-      name: 'Lapis Apple的回收站',
-      description: '又是大佬的说',
-      url: 'https://laple.top/',
+      name: `小柒`,
+      QQ: '9343981',
+      WeChat: 'http://t.cn/A6tcnVjW',
     },
   ],
   neofetch: [
     {
-      name: 'describe',
-      value: '啥都不会的屑',
+      name: 'autograph',
+      value: '世界本就不公何必徒增烦恼',
     },
     {
       name: 'UseOS',
@@ -39,34 +24,15 @@ let config = {
     },
     {
       name: 'Blog',
-      value: '<d href="https://blogging.186526.xyz">blogging.186526.xyz</d>',
-    },
-    {
-      name: 'Telegram',
-      value: '<d href="https://t.me/real186526">@real186526</d>',
+      value: '<d href="https://www.dakaliu.com">www.dakaliu.com</d>',
     },
     {
       name: 'Github',
-      value: '<d href="https://github.com/186526">@186526</d>',
+      value: '<d href="https://github.com/roinu176">@roinu176</d>',
     },
     {
       name: 'email',
-      value: '<d href="mailto:i@186526.xyz">i@186526.xyz</d>',
-    },
-    {
-      name: 'Coolapk',
-      value:
-        '<d href="https://www.coolapk.com/u/848581.">https://www.coolapk.com/u/848581.</d>',
-    },
-    {
-      name: 'GPG [i@186526.xyz]',
-      value:
-        '<d href="https://i.186526.xyz/pgp_keys.asc">https://i.186526.xyz/pgp_keys.asc</d>',
-    },
-    {
-      name: 'S/MIME Key [i@186526.xyz]',
-      value:
-        '<d href="https://i.186526.xyz/i@186526.xyz.pem">https://i.186526.xyz/i@186526.xyz.pem</d>',
+      value: '<d href="mailto:admin@dakaliu.com">admin@dakaliu.com</d>',
     },
     {
       name: 'Shell',
